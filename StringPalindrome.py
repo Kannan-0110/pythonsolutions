a0e6704a686271a657830d02f0ac4a06f7299fb6
Kannan <kannanpadmam2014@gmail.com>
2020-07-17 01:51:56 +0530

    isPalindrome() function is created
    Converted the task into a function called isPalindrome() which takes a string value/variable as parameter.

--- StringPalindrome.py
+++ StringPalindrome.py
@@ -1,5 +1,8 @@
-s=input("Enter the string: ")
-if s==s[::-1]:
-    print("The String is palindrome")
-else:
-    print("The String is not palindrome")
+isPalindrome(message):
+    if message==message[::-1]:
+        print("The String is palindrome")
+    else:
+        print("The String is not palindrome")
+
+message=input("Enter the string: ")
+isPalindrome(message)
