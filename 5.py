d34c3193ccc6c910602d8999c7b25514ef9a6fa7
Kannan <kannanpadmam2014@gmail.com>
2020-07-17 01:47:16 +0530

    Update 5.py to StringPalindrome.py
    Added a more detailed output message and input from user.

--- 5.py
+++
@@ -1,5 +0,0 @@
-s="123hllh321"
-if s==s[::-1]:
-    print("palindrome")
-else:
-    print("not palindrome")
---
+++ StringPalindrome.py
@@ -0,0 +1,5 @@
+s=input("Enter the string: ")
+if s==s[::-1]:
+    print("The String is palindrome")
+else:
+    print("The String is not palindrome")
